bb8282240d762de30d5304c1fdfcb12f8af0444e
seanbwilliams <seanbwill@gmail.com>
2022-03-09 12:47:34 +1100

    init

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Adv DSI Lab 5',
+    author='Sean Williams',
+    license='',
+)
